04bd7dfc8d5d56bc4fc7a9fce5b9388d4de74aad
dina <dinaelhadidy966@gmail.com>
2024-01-23 06:54:36 -0500

    Change formatting

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,14 +2,13 @@
 """provides some stats about Nginx logs stored in MongoDB"""
 from pymongo import MongoClient
 
-
 if __name__ == "__main__":
     mongo_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
-    print(mongo_collection.count_documents({}) + " logs")
+    print(f"{mongo_collection.count_documents({})} logs")
     print("Methods:")
     for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
         count = mongo_collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
-    get_status_count = mongo_collection.count_documents(
-            {"method": "GET", "path": "/status"})
-    print(get_status_count + " status check")
+    gets_status_count = mongo_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print(f"{gets_status_count} status check")
